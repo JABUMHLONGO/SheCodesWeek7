e1ef509c5ff2f8f16e616a263cd49eddc3d56ec2
JABUMHLONGO <121321544+JABUMHLONGO@users.noreply.github.com>
2024-08-29 16:14:28 +0200

    added weather API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature)
+}
+
+function searchCity(city) {
+  let apiKey = "34de0f88df07da2f3ea19b0tcoa504f1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(refreshWeather).catch(error => {
+    console.error('Error fetching weather data:', error.message);
+  });
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
-let searchInput = document.querySelector("#search-city");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+  let searchInput = document.querySelector("#search-city");
+  
+  searchCity(searchInput.value);
 }
 
-let searchFormElement= document.querySelector("#search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-
-
