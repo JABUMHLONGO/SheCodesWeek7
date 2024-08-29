132d17df53c4c12a7fe632a0a272eefbcc82c60c
JABUMHLONGO <121321544+JABUMHLONGO@users.noreply.github.com>
2024-08-29 16:21:28 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -28,3 +28,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Johannesburg");
